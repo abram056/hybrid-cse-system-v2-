85b7b0ae23b0a53d937a7f8fb3fff64aa4a97990
Shesham Joseph <shesham_abraham.mail@proton.me>
2025-12-01 13:01:01 +0000

    initialised project with poetry

---
+++ src/hybrid_cse_system_v2/__init__.py
@@ -0,0 +1,10 @@
+"""hybrid_cse_system_v2 package
+
+Minimal package initializer so Poetry can install the project.
+"""
+
+__version__ = "0.1.0"
+
+def hello():
+    """Example helper function."""
+    return "hybrid_cse_system_v2 initialized"
