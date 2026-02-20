7bfe0d437ebb1ce3b78088b120c41c87d84f9f97
abram056 <shesham_abraham.mail@proton.me>
2026-02-20 12:16:52 +0100

    defined tracked features for versioning

---
+++ src/hybrid_cse_system_v2/features/schema.py
@@ -0,0 +1,14 @@
+FEATURE_SCHEMA_VERSION = "1.0.0"
+
+FEATURE_NAMES = [
+    "url_count",
+    "has_url",
+    "suspicious_tld_count",
+    "phone_count",
+    "has_phone",
+    "known_smishing_phrase_hits",
+    "urgency_term_hits",
+    "reward_term_hits",
+    "text_length",
+    "token_estimate",
+]
